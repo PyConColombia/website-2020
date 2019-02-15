d2a7a61f089ae503120b96166171ea0f8a0dcc92
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2019-02-15 16:43:14 -0500

    Update build plugin

--- packages/lektor_pycon_plugin/lektor_pycon_plugin.py
+++ packages/lektor_pycon_plugin/lektor_pycon_plugin.py
@@ -3,10 +3,13 @@
 
 # Standard library imports
 from collections import OrderedDict
+import os
+import shutil
 import sys
 
 # Third party imports
 from lektor.pluginsystem import Plugin
+from lektor.reporter import reporter
 
 
 PY3 = sys.version_info[0] == 3
@@ -16,8 +19,24 @@ class PyConPlugin(Plugin):
     name = 'PyCon Custom Lektor Plugin'
     description = 'This is a custom local plugin to add extra functionality.'
 
-    DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+    COPY_FILES = ('LICENSE.txt', 'README.md')
 
     def on_setup_env(self, **extra):
         self.env.jinja_env.globals.update(dir=dir)
         self.env.jinja_env.globals.update(OrderedDict=OrderedDict)
+        self.env.jinja_env.globals.update(set=set)
+
+    def on_after_build_all(self, builder, **extra):
+        """
+        Once the build process is over, save README and LICENSE.txt to assets.
+        """
+        for f in self.COPY_FILES:
+            fpath = os.path.join(builder.env.root_path, f)
+            fbuildpath = os.path.join(builder.destination_path, f)
+
+            if os.path.isfile(fbuildpath):
+                os.remove(fbuildpath)
+
+            if os.path.isfile(fpath):
+                shutil.copy(fpath, fbuildpath)
+                reporter.report_generic("{} copied to assets folder".format(f))
