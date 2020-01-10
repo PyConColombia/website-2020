ee6055e1ea2089ecfa77b65daa677c4dc4f79752
Alejandro E. Rendon <alejandro@rendon.co>
2020-01-10 08:37:11 -0500

    Added style in responsive menu

--- assets/src/js/navbar.js
+++ assets/src/js/navbar.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
   const height = $(window).height();
 
   $('.scrollable-menu').css('max-height', height);
+
+  // Style menu responsive
+  $('.navbar-toggler').on('click', function () {
+    var openMenu = ($(".show")[0]) ? false : true;
+
+    $('.navbar').css('background-color', (openMenu) ? '#fff' : '');
+  });
 });
